fe856cbfe15ca2653ca84b35dc3f9241067e720a
Jerry Zhu <jerryzhuht@gmail.com>
2020-09-24 16:42:10 -0700

    Update features.py

--- features.py
+++ features.py
@@ -2,9 +2,11 @@
 This file contains features to be constructed and used.
 Inputs are all df datasets, outputs are all feature series.
 """
+import pandas as pd
+import numpy as np
 
 def get_features():
-    return ['Return', 'Rela_volume']
+          return ['Return', 'Rela_volume', 'vix', 'snp_return', 'gold_return']
 
 def Return(df):
     close = df['Close']
@@ -13,3 +15,18 @@ def Return(df):
 # relative volume
 def Rela_volume(df):
     volume = df['']
+    
+def vix(df):
+    return np.log(df['VIX'])
+
+def snp_return(df):
+    r = df['S&P Close']
+    return np.log(r).diff() # log return
+                  
+def gold_return(df):
+    r = df['Gold Close']
+    return np.log(r).diff() # log return
+
+def yield_10y(df):
+    return df['10y Treasury Yield']
+
