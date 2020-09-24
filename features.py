b7f61369a59b7a37f963b3f9a6c93cf4c04a271e
Yutao Shu <arthurshu@foxmail.com>
2020-09-24 15:57:28 -0700

    little changes

--- features.py
+++ features.py
@@ -4,9 +4,12 @@ Inputs are all df datasets, outputs are all feature series.
 """
 
 def get_features():
-    return ['Return']
+    return ['Return', 'Rela_volume']
 
 def Return(df):
     close = df['Close']
     return np.log(close).diff() # log return
 
+# relative volume
+def Rela_volume(df):
+    volume = df['']
--- read_data.py
+++ read_data.py
@@ -1,10 +1,11 @@
 import os 
 import pandas as pd 
+from pandas_datareader import data 
 
 def read_data(start='2012-01-01', end='2020-09-20', pathname='datasets.h5'):
-    path = "../data/" + pathname
-    if os.path.exists(path): 
-        df = pd.read_hdf(path)
+    pathname = "../230T1/data/" + pathname
+    if os.path.exists(pathname): 
+        df = pd.read_hdf(pathname)
     else:
         df = pd.DataFrame()
         df = data.DataReader("BTC-USD", start=start, end=end, data_source='yahoo')
@@ -12,4 +13,5 @@ def read_data(start='2012-01-01', end='2020-09-20', pathname='datasets.h5'):
         df['Gold Close'] = data.DataReader("GC=F", start=start, end=end, data_source='yahoo')['Adj Close']
         df['VIX'] = data.DataReader("^VIX", start=start, end=end, data_source='yahoo')['Adj Close']
         df['10y Treasury Yield'] = data.DataReader("^TNX", start=start, end=end, data_source='yahoo')['Adj Close']
+        df.to_hdf(path, key='0')
     return df 
